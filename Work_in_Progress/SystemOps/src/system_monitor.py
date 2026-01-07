569d83108db981168d33fcc5e33746bbb3d49bf5
Shaun Ray <shaun.devhub@outlook.com>
2026-01-06 21:05:55 -0500

    Add SystemOps scripts: system_monitor, network_scanner, remote_metrics, compliance_check, backup_config.ps1, check_health.sh

---
+++ Work_in_Progress/SystemOps/src/system_monitor.py
@@ -0,0 +1,16 @@
+import platform       
+import psutil 
+
+def collect_system_info(): 
+    info = {            
+        "hostname": platform.node(),       
+        "os": platform.system(),          
+        "cpu_percent": psutil.cpu_percent(interval=1), 
+        "ram_percent": psutil.virtual_memory().percent, 
+        "disk_percent": psutil.disk_usage('/').percent 
+    }
+    return info                     
+
+if __name__ == "__main__":            
+    data = collect_system_info()      
+    print(data)                    
