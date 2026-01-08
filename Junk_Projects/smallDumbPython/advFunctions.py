1e03a55bd4172680439e07550fcabca73b5afd3b
Shaun Ray <shaun.devhub@outlook.com>
2026-01-07 19:24:51 -0500

    initial commit for python junk files added from desktop to portfolio hub

---
+++ Junk_Projects/smallDumbPython/advFunctions.py
@@ -0,0 +1,74 @@
+# 26. Function that takes two strings and returns them concatenated.
+
+# def name(first, last):
+#     return first + " " + last
+
+# first = "Shaun"
+# last = "Ray"
+
+# full = name(first, last)
+# print(f"Hello, {full} how are you today?")
+
+# 27. Function that checks if a number is even, returning True/False.
+
+# def is_even(n):
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+    
+# print(is_even(4))
+# print(is_even(9))
+
+# or
+
+# def is_even(n):
+#     return n % 2 == 0
+
+# print(is_even(13))
+# print(is_even(20))
+
+# 28. Function that prints “Hello” N times, where N is an argument.
+
+# def say_hello(n):
+#     for i in range(n):
+#         print("Hello world!")
+
+# say_hello(7)
+
+# 29. Function that counts the vowels in a string.
+
+# def count_vowels(text):
+#     vowels = "aeiouAEIOU"
+#     count = 0
+
+#     for vow in text:
+#         if vow in vowels:
+#             count += 1
+#     return count
+
+# sentence = "Hello, World!"
+# print(count_vowels(sentence))
+
+# 30. Function that reverses a string.
+
+# def reverse_string(text):
+#     return text[::-1]
+
+# word = "Titanic"
+# print(reverse_string(word))
+
+# or
+
+# def reverse_string(text):
+#     reverse_string = ""
+#     for vow in text:
+#         reverse_string = vow + reverse_string
+#     return reverse_string
+
+# word = "Mayflower"
+# print(reverse_string(word))
+
+
+
+
---
+++ Junk_Projects/smallDumbPython/printInput.py
@@ -0,0 +1,25 @@
+# 1. Print “Hello, Python!”
+
+# print("Hello, python user!")
+
+# 2. Ask the user for their name and greet them.
+
+# name = input("What is your name? ")
+# print("Welcome, " + name + "!")
+
+# 3. Ask the user their favorite color and print a response.
+
+# color = input("What is your favorite color? ")
+# print("You said " + color + " is your favorite color.")
+
+# 4. Ask for two numbers and print their sum.
+
+# num1 = float(input("Choose any number: "))
+# num2 = float(input("Choose a second number: "))
+# sum = num1 + num2
+# print(f"the sum of {num1} and {num2} is {sum}")
+
+# 5. Ask the user for a sentence and print it back in uppercase.
+
+# returnCaps = input("Enter a brief sentence below: ")
+# print(returnCaps.upper())
